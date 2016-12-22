937fb030fa9163f1394a6df94a0c254a271f2c54
Salomon <salomonpluviose@yahoo.com>
2016-12-21 19:34:13 -0500

    messed up name

--- dummy/ViewController.swift
+++ dummy/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     let name = "salomon"
-    let newName = "Davone"
+    let newName = "weweas"
 
     override func viewDidLoad() {
         super.viewDidLoad()
